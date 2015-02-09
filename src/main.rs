714047466d513c6c02494bad4eb37f1a8c21bc44
Zack Slayton <zack.slayton@gmail.com>
2015-02-09 14:13:17 -0500

    stomp-rs v0.8.0 compatibility.

--- src/main.rs
+++ src/main.rs
@@ -1,32 +1,43 @@
-extern crate stomp;
-use stomp::frame::Frame;
-use stomp::subscription::{Ack, AckOrNack, Auto};
+#![feature(collections)]
+#![feature(core)]
+#![feature(std_misc)]
 
-const NUMBER_OF_THREADS : uint = 10;
+extern crate stomp;
+
+use std::sync::mpsc::{Sender, Receiver, channel, RecvError};
+use std::thread::Thread;
+use stomp::frame::Frame;
+use stomp::subscription::AckOrNack;
+
+const NUMBER_OF_THREADS : u32 = 10;
+const REPORT_EVERY : u64 = 1000;
 const TOPIC : &'static str = "/queue/QUEUE_NAME";
 
 fn main() {
-  
-  fn on_message(frame: &Frame) -> AckOrNack {
-    Ack
-  }
+  let mut receivers : Vec<Receiver<()>> = Vec::with_capacity(NUMBER_OF_THREADS as usize);
 
-  let mut receivers : Vec<Receiver<()>> = Vec::with_capacity(NUMBER_OF_THREADS);
-
-  for thread_number in range(0, NUMBER_OF_THREADS) {
+  for thread_number in range(0u32, NUMBER_OF_THREADS) {
     let (tx, rx): (Sender<()>, Receiver<()>) = channel();
     receivers.push(rx);
-    spawn(proc() {
-      let mut session = match stomp::connect("127.0.0.1", 61613) {
-        Ok(session)  => session,
+    Thread::spawn(move || {
+      let mut message_count : u64 = 0;
+      let mut session = match stomp::session("127.0.0.1", 61613).start() {
+        Ok(session) => session,
         Err(error) => panic!("Could not connect to the server: {}", error)
       };
-      session.subscribe(TOPIC, Auto, on_message);
-      println!("Session #{} created. Subscribed to {}", thread_number, TOPIC);
+      match session.subscription(TOPIC, |frame: &Frame|{
+        message_count += 1;
+        if message_count % REPORT_EVERY == 0 {
+          println!("Thread #{} -> {}", thread_number, message_count);
+        }
+        AckOrNack::Ack
+      }).start() {
+        Ok(_) => println!("Thread #{} -> subscribed successfully.", thread_number),
+        Err(error) => println!("Thread #{} -> Failed to subscribe: {}", thread_number, error)
+      }
       session.listen(); // Loops infinitely, awaiting messages
       tx.send(());
     });
   }
-
-  let _ = receivers.iter().map(|rx| (*rx).recv());
+  let results : Vec<Result<(), RecvError>> = receivers.iter().map(|rx| (*rx).recv()).collect();
 }
